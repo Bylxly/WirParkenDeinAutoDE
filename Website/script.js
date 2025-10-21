e7a11e369e3625c098c445b2768366c6ae94b4d2
Fitz <tyler.hoernig83@gmail.com>
2025-10-21 10:33:58 +0200

    html und js update

--- Website/script.js
+++ Website/script.js
@@ -1,11 +1,27 @@
-const totalSpots = 5;
+const SpotsZ1 = 23;
+const SpotsZ2 = 18;
+const SpotsZ3 = 18;
+/**
+fetch("daten.json")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.name);
+    data.punkte.forEach(p => {
+      console.log(`x=${p.x}, y=${p.y}`);
+    });
+  })
+  .catch(error => console.error("Fehler:", error));
+*/
 
 //aktualisieren der Belegung
 function updateParkingData() {
-  for (let i = 1; i <= totalSpots; i++) {
-    const spot = document.getElementById(`P${i}`);
-    const occupied = Math.random() > 0.6;
-    spot.setAttribute('fill', occupied ? '#e53935' : '#4caf50');
+  for (let z = 1; z <= 3; z++) {
+    const totalSpots = eval(`SpotsZ${z}`);
+    for (let i = 1; i <= totalSpots; i++) {
+      const spot = document.getElementById(`z${z}p${i}`);
+      const occupied = Math.random() > 0.6;
+      spot.setAttribute('fill', occupied ? '#e53935' : '#4caf50');
+    }
   }
 }
 
